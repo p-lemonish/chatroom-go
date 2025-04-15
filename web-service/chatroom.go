73c4dfc41c29b4078dbc67d24f7ac276850830bd
Lemonish <patrikmihelson@gmail.com>
2025-04-15 22:13:24 +0300

    testing websockets with gorilla-websocket

--- web-service/chatroom.go
+++ web-service/chatroom.go
@@ -1,13 +1,67 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/gorilla/websocket"
 )
 
 type user struct {
 	Username string `json:"username"`
+}
+
+type Client struct {
+	conn *websocket.Conn
+	send chan []byte
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.conn.Close()
+	}()
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			log.Printf("read error: %v", err)
+			break
+		}
+		c.send <- message
+	}
+}
+
+func (c *Client) writePump() {
+	defer func() {
+		c.conn.Close()
+	}()
+	for msg := range c.send {
+		err := c.conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			log.Printf("write error: %v", err)
+			break
+		}
+	}
+}
+
+func handleWebsocket(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Printf("upgrade error: %v", err)
+		return
+	}
+	client := &Client{
+		conn: conn,
+		send: make(chan []byte, 256),
+	}
+	go client.writePump()
+	go client.readPump()
 }
 
 var users = []user{}
@@ -34,5 +88,9 @@ func main() {
 	r.Use(cors.New(config))
 	r.GET("/users", getUsers)
 	r.POST("/start", postUser)
+	r.GET("/main", func(c *gin.Context) {
+		handleWebsocket(c)
+	})
+	fmt.Println("WS started at 8080")
 	r.Run()
 }
