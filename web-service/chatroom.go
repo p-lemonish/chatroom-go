13e9a3bdb2d50334e6b69e34a52a6d5fd594392f
Lemonish <patrikmihelson@gmail.com>
2025-04-18 22:05:42 +0300

    learned more about websockets.
    got user join/leave recorded to the chat. also reworked message to json
    format holding type of msg (auth/message), username, text

--- web-service/chatroom.go
+++ web-service/chatroom.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -15,25 +15,96 @@ type user struct {
 }
 
 type Client struct {
+	hub  *Hub
 	conn *websocket.Conn
 	send chan []byte
+	user *user
+}
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+var users = []user{}
+var hub = newHub()
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				client.hub.broadcast <- []byte(client.user.Username + " has left the chat!")
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
 }
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
+type Message struct {
+	Type     string `json:"type"`
+	Username string `json:"username"`
+	Text     string `json:"text"`
+}
+
 func (c *Client) readPump() {
 	defer func() {
+		c.hub.unregister <- c
 		c.conn.Close()
 	}()
 	for {
-		_, message, err := c.conn.ReadMessage()
+		var msg Message
+		err := c.conn.ReadJSON(&msg)
 		if err != nil {
 			log.Printf("read error: %v", err)
 			break
 		}
-		c.send <- message
+		switch {
+		case msg.Type == "auth":
+			if msg.Username != "" {
+				pieces := strings.SplitN(msg.Username, "_", -1)
+				for idx, piece := range pieces {
+					if piece == "supersecretmessage" {
+						msg.Username = strings.Join(pieces[:idx], "")
+					}
+				}
+				c.user.Username = msg.Username
+				c.hub.broadcast <- []byte(c.user.Username + " has joined the chat!")
+			} else {
+				c.user.Username = "anonymous"
+				log.Println("Didn't find username in auth call. Setting user as anonymous")
+			}
+		case msg.Type == "message":
+			message := append([]byte(c.user.Username+": "), []byte(msg.Text)...)
+			c.hub.broadcast <- message
+		}
 	}
 }
 
@@ -41,44 +112,51 @@ func (c *Client) writePump() {
 	defer func() {
 		c.conn.Close()
 	}()
-	for msg := range c.send {
-		err := c.conn.WriteMessage(websocket.TextMessage, msg)
+	for {
+		message, ok := <-c.send
+		if !ok {
+			c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+			return
+		}
+		w, err := c.conn.NextWriter(websocket.TextMessage)
 		if err != nil {
-			log.Printf("write error: %v", err)
-			break
+			return
+		}
+		w.Write(message)
+		if err := w.Close(); err != nil {
+			return
 		}
 	}
 }
 
-func handleWebsocket(c *gin.Context) {
-	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+func handleWebsocket(ctx *gin.Context) {
+	go hub.run()
+	conn, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
 	if err != nil {
 		log.Printf("upgrade error: %v", err)
 		return
 	}
 	client := &Client{
+		hub:  hub,
 		conn: conn,
 		send: make(chan []byte, 256),
+		user: &user{},
 	}
+	client.hub.register <- client
 	go client.writePump()
 	go client.readPump()
 }
 
-var users = []user{}
-
-func getUsers(c *gin.Context) {
-	c.JSON(http.StatusOK, users)
+func getUsers(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, users)
 }
 
-func postUser(c *gin.Context) {
+func postUser(ctx *gin.Context) {
 	var newUser user
-	if err := c.BindJSON(&newUser); err != nil {
+	if err := ctx.BindJSON(&newUser); err != nil {
 		return
 	}
 	users = append(users, newUser)
-	c.JSON(http.StatusCreated, gin.H{
-		"message": "started chat with user " + newUser.Username,
-	})
 }
 
 func main() {
@@ -88,9 +166,8 @@ func main() {
 	r.Use(cors.New(config))
 	r.GET("/users", getUsers)
 	r.POST("/start", postUser)
-	r.GET("/main", func(c *gin.Context) {
-		handleWebsocket(c)
+	r.GET("/main", func(ctx *gin.Context) {
+		handleWebsocket(ctx)
 	})
-	fmt.Println("WS started at 8080")
 	r.Run()
 }
