7e7adba896ca0749025569ee8222a082e608d53a
Lemonish <patrikmihelson@gmail.com>
2025-04-22 18:18:13 +0300

    updates to env handling

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -15,11 +17,11 @@ TODOs
 func main() {
 	r := gin.Default()
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{
-		"http://localhost:5173",
-		"http://18.192.11.118",
-		"http://18.192.11.118:80",
-		"http://100.27.185.143:80"}
+	allowed := os.Getenv("ALLOWED_ORIGIN")
+	if allowed == "" {
+		allowed = "http://localhost:5173"
+	}
+	config.AllowOrigins = []string{allowed}
 	r.Use(cors.New(config))
 	r.GET("/users", getUsers)
 	r.POST("/start", postUser)
